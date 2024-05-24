525ee5ed32b87c9a0de543f07a5f05ce6ba96268
prolgrammer <plotnikov.va2004@gmail.com>
2024-05-24 15:00:41 +0400

    Init

---
+++ Domain/Entities/Part.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Domain.Entities
+{
+    public class Part
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+        [Display(Name = "Категория")]
+        public string Category { get; set; }
+
+        [Display(Name = "Цена (руб)")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Количество на складе")]
+        public int? Quantity { get; set; }
+    }
+}
---
+++ Domain/Entities/ShippingDetail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class ShippingDetail
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Укажите как вас зовут")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Вставьте первый адрес доставки")]
+        [Display(Name = "Адрес адрес")]
+        public string Adress { get; set; }
+
+        [Display(Name = "Номер телефона")]
+        public string Number { get; set; }
+
+        [Required(ErrorMessage = "Укажите почту")]
+        [Display(Name = "Почта")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Вы ввели некорректный email")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Укажите город")]
+        [Display(Name = "Город")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Укажите страну")]
+        [Display(Name = "Страна")]
+        public string Country { get; set; }
+
+        public string PartAndAmount { get; set; }
+
+       
+    }
+}
